28af4f6601b2cb4940dfda33da48d64ec27b4b87
kobadze888 <mr.kobadze@gmail.com>
2025-11-03 14:11:31 +0400

    Fix: Disable CSS modules hash/prefix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // --- ЭТО ИСПРАВЛЕНИЕ ---
+  // Мы явно отключаем "CSS Modules", которые коверкают имена
+  // классов (добавляя префиксы типа `_grid` или хэши).
+  css: {
+    modules: {
+      // Говорим Vite, что все CSS-файлы по умолчанию - глобальные,
+      // а не "модули".
+      scopeBehaviour: 'global',
+      // Говорим не изменять имена классов, а использовать их "как есть"
+      generateScopedName: '[local]',
+    }
+  }
 })
